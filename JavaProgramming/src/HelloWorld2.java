e13470b70a207056ca98dd56d88049baa337e202
JINBYEUNGKU1 <splendid7373@gmail.com>
2016-08-17 16:03:45 +0900

    HelloWorld2 추가

---
+++ JavaProgramming/src/HelloWorld2.java
@@ -0,0 +1,8 @@
+public class HelloWorld2 {
+	public static void main(String[] args) {
+		System.out.println("A");
+		System.out.println("B");
+		System.out.println("C");
+		System.out.println("D");
+	}
+}
