fadaaf18d7be283b1ad2f6084adeb295b3e1cf43
JinByeungKu <splendid1014@naver.com>
2016-08-17 23:37:54 +0900

    HelloWorld 수정

--- JavaProgramming/src/HelloWorld.java
+++ JavaProgramming/src/HelloWorld.java
@@ -3,6 +3,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("A");
 		System.out.println("B");
+		System.out.println("C");
+		System.out.println("D");
 
 	}
 
