f2f3d4202d7e811dbc4bb26def484efeeb6315f1
JINBYEUNGKU1 <splendid7373@gmail.com>
2016-08-17 15:34:42 +0900

    HelloWorld2 삭제함

--- JavaProgramming/src/HelloWorld2.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld2 {
-	public static void main(String[] args) {
-		System.out.println("A");
-		System.out.println("B");
-		System.out.println("C");
-		System.out.println("D");
-	}
-}
