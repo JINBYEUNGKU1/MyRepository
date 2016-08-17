3cd9a9f1260f4f9000710eb66685761dc59da200
JINBYEUNGKU1 <splendid7373@gmail.com>
2016-08-17 16:27:28 +0900

    HelloWorld5 추가

--- JavaProgramming/src/HelloWorld3.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld3 {
-	public static void main(String[] args) {
-		System.out.println("A");
-		System.out.println("B");
-		System.out.println("C");
-		System.out.println("D");
-		System.out.println("E");
-	}
-}
--- JavaProgramming/src/HelloWorld5.java
+++ JavaProgramming/src/HelloWorld5.java
@@ -1,10 +1,8 @@
-public class HelloWorld4 {
+public class HelloWorld5 {
 	public static void main(String[] args) {
 		System.out.println("A");
 		System.out.println("B");
-		System.out.println("C");
 		System.out.println("D");
 		System.out.println("E");
-		System.out.println("F");
 	}
 }
