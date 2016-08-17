ea7fe2d8d87f8b817518ed791d8bdcb1eb3565d1
JINBYEUNGKU1 <splendid7373@gmail.com>
2016-08-17 16:30:00 +0900

    HelloWorld 수정

--- JavaProgramming/src/HelloWorld.java
+++ JavaProgramming/src/HelloWorld.java
@@ -2,7 +2,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("A");
 		System.out.println("B");
-		System.out.println("C");
 		System.out.println("D");
 		System.out.println("E");
 	}
--- JavaProgramming/src/HelloWorld5.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld5 {
-	public static void main(String[] args) {
-		System.out.println("A");
-		System.out.println("B");
-		System.out.println("D");
-		System.out.println("E");
-	}
-}
